d292430d3b140ebe7d0e0fc1bc02270ba8dc1e6d
Galih Indra Firmansyah <galihindra2401@gmail.com>
2023-07-11 08:38:46 +0700

    feat: slicing onboarding

--- app/src/main/java/com/giftech/nutasel/ui/NutaselApp.kt
+++ app/src/main/java/com/giftech/nutasel/ui/NutaselApp.kt
@@ -6,6 +6,8 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.giftech.nutasel.ui.components.enums.HeroEnum
+import com.giftech.nutasel.ui.components.templates.Onboarding
 import com.giftech.nutasel.ui.pages.InputDataDiriScreen
 import com.giftech.nutasel.ui.route.Screen
 
@@ -18,8 +20,46 @@ fun NutaselApp() {
         startDestination = Screen.InputDataDiri.route
     ) {
         composable(Screen.InputDataDiri.route){
-            InputDataDiriScreen()
+            InputDataDiriScreen(
+                onNext = {
+                    navHostController.navigate(Screen.OnboardingASAQ1.route)
+                }
+            )
         }
+        composable(Screen.OnboardingASAQ1.route){
+            Onboarding(
+                onBack = {
+                    navHostController.popBackStack()
+                },
+                onNext = {
+                    navHostController.navigate(Screen.OnboardingASAQ2.route)
+                },
+                hero = HeroEnum.AsaqOnboard1
+            )
+        }
+        composable(Screen.OnboardingASAQ2.route){
+            Onboarding(
+                onBack = {
+                    navHostController.popBackStack()
+                },
+                onNext = {
+                    navHostController.navigate(Screen.OnboardingASAQ3.route)
+                },
+                hero = HeroEnum.AsaqOnboard2
+            )
+        }
+        composable(Screen.OnboardingASAQ3.route){
+            Onboarding(
+                onBack = {
+                    navHostController.popBackStack()
+                },
+                onNext = {
+                    navHostController.navigate(Screen.InputDataDiri.route)
+                },
+                hero = HeroEnum.AsaqOnboard3
+            )
+        }
+
     }
 }
 
--- app/src/main/java/com/giftech/nutasel/ui/route/Screen.kt
+++ app/src/main/java/com/giftech/nutasel/ui/route/Screen.kt
@@ -2,4 +2,7 @@ package com.giftech.nutasel.ui.route
 
 sealed class Screen(val route:String){
     object InputDataDiri:Screen("InputDataDiri")
+    object OnboardingASAQ1:Screen("OnboardingASAQ1")
+    object OnboardingASAQ2:Screen("OnboardingASAQ2")
+    object OnboardingASAQ3:Screen("OnboardingASAQ3")
 }
