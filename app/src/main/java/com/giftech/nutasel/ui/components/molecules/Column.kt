9c35268be24114ed491b0ced5faef89ba3b57d50
Galih Indra Firmansyah <galihindra2401@gmail.com>
2023-07-11 08:34:33 +0700

    refactor: change parameter to heroenum

--- app/src/main/java/com/giftech/nutasel/ui/components/molecules/Column.kt
+++ app/src/main/java/com/giftech/nutasel/ui/components/molecules/Column.kt
@@ -1,6 +1,5 @@
 package com.giftech.nutasel.ui.components.molecules
 
-import androidx.annotation.DrawableRes
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -13,31 +12,29 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import com.giftech.nutasel.R
+import com.giftech.nutasel.ui.components.enums.HeroEnum
 
 @Composable
 fun HeroColumn(
     modifier: Modifier = Modifier,
-    @DrawableRes image: Int,
-    title: String,
-    description: String
+    hero:HeroEnum
 ) {
     Column(
         modifier = modifier.fillMaxWidth(),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Image(
-            painter = painterResource(image),
+            painter = painterResource(hero.image),
             contentDescription = ""
         )
         Text(
-            text = title,
+            text = hero.title,
             style = MaterialTheme.typography.titleLarge,
             modifier = Modifier.padding(16.dp),
             textAlign = TextAlign.Center
         )
         Text(
-            text = description,
+            text = hero.description,
             style = MaterialTheme.typography.bodySmall,
             textAlign = TextAlign.Center
         )
