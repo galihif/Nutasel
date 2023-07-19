edb278927dc9e989e01ce91d6d97c249eeb802b3
Galih Indra Firmansyah <galihindra2401@gmail.com>
2023-07-19 16:51:44 +0700

    feat: change filterchip to elevated chip

--- app/src/main/java/com/giftech/nutasel/ui/components/atoms/Chips.kt
+++ app/src/main/java/com/giftech/nutasel/ui/components/atoms/Chips.kt
@@ -5,8 +5,8 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Check
+import androidx.compose.material3.ElevatedFilterChip
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.FilterChip
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -21,14 +21,14 @@ fun MyFilterChips(
     onSelectedChange: (Boolean) -> Unit,
     text: String
 ) {
-    FilterChip(
+    ElevatedFilterChip(
         modifier = modifier.fillMaxWidth(),
         selected = selected,
         onClick = { onSelectedChange(!selected) },
         label = {
             Text(
                 text = text,
-                modifier = Modifier.padding(8.dp)
+                modifier = Modifier.padding(16.dp),
             )
         },
         leadingIcon = {
@@ -38,6 +38,6 @@ fun MyFilterChips(
                     ""
                 )
             }
-        }
+        },
     )
 }
