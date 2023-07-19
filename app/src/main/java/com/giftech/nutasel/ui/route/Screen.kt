15d7e37a64a330a2c17087569194f1bf4e8bd274
Galih Indra Firmansyah <galihindra2401@gmail.com>
2023-07-19 16:56:42 +0700

    feat: add ASAQ1 route

--- app/src/main/java/com/giftech/nutasel/ui/route/Screen.kt
+++ app/src/main/java/com/giftech/nutasel/ui/route/Screen.kt
@@ -5,4 +5,5 @@ sealed class Screen(val route:String){
     object OnboardingASAQ1:Screen("OnboardingASAQ1")
     object OnboardingASAQ2:Screen("OnboardingASAQ2")
     object OnboardingASAQ3:Screen("OnboardingASAQ3")
+    object  ASAQ1:Screen("ASAQ1")
 }
