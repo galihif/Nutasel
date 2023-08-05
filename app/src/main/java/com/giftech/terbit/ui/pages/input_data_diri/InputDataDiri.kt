258c287b9cb283d0ad4a8e1a75785334c2e002b1
Galih Indra Firmansyah <galihindra2401@gmail.com>
2023-08-05 21:27:05 +0700

    feat: pass user data

--- app/src/main/java/com/giftech/terbit/ui/pages/input_data_diri/InputDataDiri.kt
+++ app/src/main/java/com/giftech/terbit/ui/pages/input_data_diri/InputDataDiri.kt
@@ -9,20 +9,26 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.core.text.isDigitsOnly
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.giftech.terbit.data.model.User
 import com.giftech.terbit.ui.components.atoms.MyFilterChips
 import com.giftech.terbit.ui.components.atoms.MyOutlinedTextField
 import com.giftech.terbit.ui.components.atoms.PrimaryButton
@@ -32,7 +38,7 @@ import com.giftech.terbit.ui.components.molecules.HeroColumn
 @ExperimentalMaterial3Api
 @Composable
 fun InputDataDiriScreen(
-    onNext: () -> Unit,
+    onNext: (User) -> Unit,
     viewModel: InputDataDiriViewModel = hiltViewModel()
 ) {
     val scrollState = rememberScrollState()
@@ -40,8 +46,13 @@ fun InputDataDiriScreen(
     val nama by remember { viewModel.nama }.collectAsState()
     val tinggi by remember { viewModel.tinggi }.collectAsState()
     val berat by remember { viewModel.berat }.collectAsState()
-    val tglLahir by remember { viewModel.tglLahir }.collectAsState()
+    val usia by remember { viewModel.usia }.collectAsState()
     val isMale by remember { viewModel.isMale }.collectAsState()
+    var isValid by remember { mutableStateOf(false) }
+    LaunchedEffect(nama, tinggi, berat, usia) {
+        isValid =
+            nama.isNotBlank() && tinggi.isNotBlank() && berat.isNotBlank() && usia.isNotBlank()
+    }
     Scaffold { padding ->
         Column(
             modifier = Modifier
@@ -76,7 +87,10 @@ fun InputDataDiriScreen(
                     },
                     label = "Berat",
                     supportingText = "Kilogram (Kg)",
-                    modifier = Modifier.weight(1f)
+                    modifier = Modifier.weight(1f),
+                    keyboardOptions = KeyboardOptions.Default.copy(
+                        keyboardType = KeyboardType.Number
+                    )
                 )
                 Spacer(Modifier.width(24.dp))
                 MyOutlinedTextField(
@@ -88,14 +102,17 @@ fun InputDataDiriScreen(
                     },
                     label = "Tinggi",
                     supportingText = "Centimeter (Cm)",
-                    modifier = Modifier.weight(1f)
+                    modifier = Modifier.weight(1f),
+                    keyboardOptions = KeyboardOptions.Default.copy(
+                        keyboardType = KeyboardType.Number
+                    )
                 )
             }
             MyOutlinedTextField(
-                value = tglLahir,
-                onValueChange = { viewModel.tglLahir.value = it },
-                label = "Tanggal Lahir",
-                supportingText = "Bulan, Tanggal, Tahun",
+                value = usia,
+                onValueChange = { viewModel.usia.value = it },
+                label = "Usia",
+                supportingText = "Tahun",
             )
             Column(verticalArrangement = Arrangement.spacedBy(16.dp)) {
                 Text(
@@ -119,8 +136,19 @@ fun InputDataDiriScreen(
             }
             PrimaryButton(
                 text = "Selanjutnya",
-                onClick = onNext,
-                modifier = Modifier.fillMaxWidth()
+                onClick = {
+                    onNext(
+                        User(
+                            nama = nama,
+                            tinggi = tinggi.trim().toInt(),
+                            berat = berat.trim().toInt(),
+                            usia = usia.trim().toInt(),
+                            isMale = isMale
+                        )
+                    )
+                },
+                modifier = Modifier.fillMaxWidth(),
+                enabled = isValid
             )
         }
     }
