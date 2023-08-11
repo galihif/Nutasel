b6579b9409fab51a8c95977e82782e299e11223f
Galih Indra Firmansyah <galihindra2401@gmail.com>
2023-08-11 14:50:24 +0700

    refactor: delete viewmodel

--- app/src/main/java/com/giftech/terbit/ui/pages/hasil_imt/HasilIMT.kt
+++ app/src/main/java/com/giftech/terbit/ui/pages/hasil_imt/HasilIMT.kt
@@ -1,12 +1,6 @@
 package com.giftech.terbit.ui.pages.hasil_imt
 
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.key
-import androidx.compose.runtime.remember
-import androidx.hilt.navigation.compose.hiltViewModel
 import com.giftech.terbit.data.model.User
 import com.giftech.terbit.ui.components.enums.HeroEnum
 import com.giftech.terbit.ui.components.templates.Hasil
@@ -17,34 +11,20 @@ fun HasilIMTScreen(
     onNext: (User) -> Unit = {},
     onBack: () -> Unit = {},
     user: User,
-    viewModel: HasilIMTViewModel = hiltViewModel()
 ) {
-    LaunchedEffect(user) {
-        viewModel.setUser(user)
-    }
-    val skorIMT by remember{
-        viewModel.skorIMT
-    }.collectAsState()
-
-    val kategoriIMT by remember {
-        viewModel.kategoriIMT
-    }.collectAsState()
-
-    key(skorIMT, kategoriIMT) {
-        Hasil(
-            onNext = {
-                onNext(user.copy(skorIMT = skorIMT))
-            },
-            onBack = onBack,
-            hero = HeroEnum.HasilIMT,
-            statusColor = kategoriIMT.color,
-            nama = user.nama,
-            skorTitle = "Skor IMT",
-            skor = skorIMT.toFormattedString(),
-            kategoriTitle = "KategoriMT",
-            kategori = kategoriIMT.title,
-            desc = kategoriIMT.desc(skorIMT.toFormattedString()),
-            buttonText = "Cek Status Gizi"
-        )
-    }
+    Hasil(
+        onNext = {
+            onNext(user)
+        },
+        onBack = onBack,
+        hero = HeroEnum.HasilIMT,
+        statusColor = user.kategoriIMT.color,
+        nama = user.nama,
+        skorTitle = "Skor IMT",
+        skor = user.skorIMT.toFormattedString(),
+        kategoriTitle = "KategoriMT",
+        kategori = user.kategoriIMT.title,
+        desc = user.kategoriIMT.desc(user.skorIMT.toFormattedString()),
+        buttonText = "Cek Status Gizi"
+    )
 }
