e34a9a9ffe1eac72f01eeebe41c4ecfaa5dc76e6
Galih Indra Firmansyah <galihindra2401@gmail.com>
2023-08-22 12:47:21 +0700

    refactor: delete StatusGizi

--- app/src/main/java/com/giftech/terbit/ui/route/Screen.kt
+++ app/src/main/java/com/giftech/terbit/ui/route/Screen.kt
@@ -4,8 +4,6 @@ sealed class Screen(val route:String){
     object InputDataDiri:Screen("InputDataDiri")
     object OnboardingIMT:Screen("OnboardingIMT")
     object HasilIMT:Screen("HasilIMT")
-    object OnboardingStatusGizi:Screen("OnboardingStatusGizi")
-    object HasilStatusGizi:Screen("HasilStatusGizi")
     object OnboardingASAQ1:Screen("OnboardingASAQ1")
     object OnboardingASAQ2:Screen("OnboardingASAQ2")
     object OnboardingASAQ3:Screen("OnboardingASAQ3")
