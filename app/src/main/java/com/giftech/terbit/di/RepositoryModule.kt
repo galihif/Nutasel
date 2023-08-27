8704b5b25eb2fb58607c03bca8c15f682c3447fd
Galih Indra Firmansyah <galihindra2401@gmail.com>
2023-08-27 15:49:43 +0700

    di: create DataStoreModule

--- app/src/main/java/com/giftech/terbit/di/RepositoryModule.kt
+++ app/src/main/java/com/giftech/terbit/di/RepositoryModule.kt
@@ -3,15 +3,17 @@ package com.giftech.terbit.di
 import com.giftech.terbit.data.repository.FfqFoodCategoryRepository
 import com.giftech.terbit.data.repository.FfqQuestionRepository
 import com.giftech.terbit.data.repository.ProgramRepository
+import com.giftech.terbit.data.repository.UserRepository
 import com.giftech.terbit.domain.repository.IFfqFoodCategoryRepository
 import com.giftech.terbit.domain.repository.IFfqQuestionRepository
 import com.giftech.terbit.domain.repository.IProgramRepository
+import com.giftech.terbit.domain.repository.IUserRepository
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 
-@Module(includes = [DatabaseModule::class])
+@Module(includes = [DatabaseModule::class, DataStoreModule::class])
 @InstallIn(SingletonComponent::class)
 abstract class RepositoryModule {
     
@@ -29,5 +31,10 @@ abstract class RepositoryModule {
     abstract fun provideProgramRepository(
         programRepository: ProgramRepository,
     ): IProgramRepository
+
+    @Binds
+    abstract fun provideUserRepository(
+        userRepository: UserRepository,
+    ): IUserRepository
     
 }
