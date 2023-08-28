d5b775bbcee7ce60c264529b2034561d5dab9899
Arief Zuhri <ariefzuhri72@gmail.com>
2023-08-28 18:42:30 +0700

    refactor: delete double constants class

--- app/src/main/java/com/giftech/terbit/ui/utils/Constants.kt
+++ app/src/main/java/com/giftech/terbit/ui/utils/Constants.kt
@@ -2,6 +2,8 @@ package com.giftech.terbit.ui.utils
 
 object Constants {
     
+    const val TOTAL_ASAQ = 12
+    
     object EXTRAS {
         const val PROGRAM_ID = "program_id"
         const val FFQ_FOOD_CATEGORY_ID = "ffq_food_category_id"
