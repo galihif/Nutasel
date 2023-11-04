8919c1b58bea38364124cb7f2184260b9fc45ca4
Arief Zuhri <ariefzuhri72@gmail.com>
2023-11-04 11:04:10 +0700

    domain: store pre-test and post-test break days as constants

--- app/src/main/java/com/giftech/terbit/domain/util/Constants.kt
+++ app/src/main/java/com/giftech/terbit/domain/util/Constants.kt
@@ -3,15 +3,26 @@ package com.giftech.terbit.domain.util
 object Constants {
     
     object ProgramId {
+        
         const val FIRST_ASAQ = 1
         const val FIRST_FFQ = 2
         const val LAST_ASAQ = 39
         const val LAST_FFQ = 40
+        
     }
     
     object DatePattern {
+        
         const val READABLE_DEFAULT = "dd - MM - yyyy"
         const val FILE_NAME = "ddMMyyyy HHmmss"
+        
     }
-
+    
+    object BreakDays {
+        
+        const val AFTER_PRE_TEST = 3L
+        const val BEFORE_POST_TEST = 3L
+        
+    }
+    
 }
