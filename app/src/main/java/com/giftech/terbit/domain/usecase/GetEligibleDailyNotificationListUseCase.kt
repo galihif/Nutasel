8b3b4d769c4338c76696ac027558b3841ae82806
Arief Zuhri <ariefzuhri72@gmail.com>
2023-11-05 09:23:04 +0700

    domain: enable daily reminder to take programs only when pre-test and all daily programs are completed

---
+++ app/src/main/java/com/giftech/terbit/domain/usecase/GetEligibleDailyNotificationListUseCase.kt
@@ -0,0 +1,48 @@
+package com.giftech.terbit.domain.usecase
+
+import com.giftech.terbit.data.repository.ProgramRepository
+import com.giftech.terbit.domain.enums.NotificationType
+import com.giftech.terbit.domain.enums.ProgramTag
+import com.giftech.terbit.domain.model.Notification
+import com.giftech.terbit.domain.repository.INotificationRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flatMapLatest
+import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.flow.mapLatest
+import javax.inject.Inject
+
+class GetEligibleDailyNotificationListUseCase @Inject constructor(
+    private val notificationRepository: INotificationRepository,
+    private val programRepository: ProgramRepository,
+) {
+    
+    @OptIn(ExperimentalCoroutinesApi::class)
+    suspend operator fun invoke(): Flow<List<Notification>> {
+        return programRepository.getAll()
+            .flatMapLatest { programList ->
+                notificationRepository.getAll()
+                    .mapLatest { notificationList ->
+                        val preTestProgramList = programList.filter {
+                            it.tag == ProgramTag.PRE_TEST
+                        }
+                        val weeklyProgramList = programList.filter {
+                            it.tag == ProgramTag.WEEKLY_PROGRAM
+                        }
+                        val isPreTestDone = preTestProgramList.all { it.isCompleted }
+                        val isWeeklyProgramDone = weeklyProgramList.all { it.isCompleted }
+                        
+                        notificationList.filter {
+                            when (it.id) {
+                                6000 -> isPreTestDone && !isWeeklyProgramDone
+                                else -> it.type == NotificationType.DAILY_TIPS
+                            }
+                        }
+                        
+                    }
+                    .flowOn(Dispatchers.IO)
+            }
+    }
+    
+}
