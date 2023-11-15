f0173301774fa1d47dbb2b186b9a83f30c333854
Arief Zuhri <ariefzuhri72@gmail.com>
2023-11-15 08:13:34 +0700

    domain: disable fill monitoring notifications when the program has not been started

--- app/src/main/java/com/giftech/terbit/domain/usecase/GetEligibleDailyNotificationListUseCase.kt
+++ app/src/main/java/com/giftech/terbit/domain/usecase/GetEligibleDailyNotificationListUseCase.kt
@@ -5,12 +5,15 @@ import com.giftech.terbit.domain.enums.ProgramTag
 import com.giftech.terbit.domain.model.Notification
 import com.giftech.terbit.domain.repository.INotificationRepository
 import com.giftech.terbit.domain.repository.IProgramRepository
+import com.giftech.terbit.domain.util.Constants
+import com.giftech.terbit.domain.util.toLocalDateTime
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flatMapLatest
 import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.flow.mapLatest
+import java.time.LocalDate
 import javax.inject.Inject
 
 class GetEligibleDailyNotificationListUseCase @Inject constructor(
@@ -31,11 +34,15 @@ class GetEligibleDailyNotificationListUseCase @Inject constructor(
                             it.tag == ProgramTag.WEEKLY_PROGRAM
                         }
                         val isPreTestDone = preTestProgramList.all { it.isCompleted }
+                        val isWeeklyProgramStarted = LocalDate.now() >=
+                                preTestProgramList.first().completionDateInMillis
+                                    .toLocalDateTime().toLocalDate()
+                                    .plusDays(Constants.BreakDays.AFTER_PRE_TEST)
                         val isWeeklyProgramDone = weeklyProgramList.all { it.isCompleted }
                         
                         notificationList.filter {
                             when (it.id) {
-                                6000 -> isPreTestDone && !isWeeklyProgramDone
+                                6000 -> isPreTestDone && isWeeklyProgramStarted && !isWeeklyProgramDone
                                 else -> it.type == NotificationType.DAILY_TIPS
                             }
                         }
